1f32d624eb01b6ee5e34df6cad3f2262ec462e10
Enekovaz4 <Enekovaz4@gmail.com>
2025-08-16 13:06:49 +0200

    v0.1 - Knowledge page + fixes

--- old/basicCss.php
+++
@@ -1,9 +0,0 @@
-
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-
-<!-- //BOOTSTRAP 4 -->
-
-
-<!-- METROUI -->
-<link rel="stylesheet" href="https://cdn.metroui.org.ua/v4/css/metro-all.min.css">
-<!-- //METROUI -->
