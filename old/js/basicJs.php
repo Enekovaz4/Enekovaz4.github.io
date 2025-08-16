589f567bd19724250aa814ae11d9898bef52bcb2
Enekovaz4 <Enekovaz4@gmail.com>
2025-08-16 13:08:57 +0200

    restore OLD page

---
+++ old/js/basicJs.php
@@ -0,0 +1,32 @@
+<!--JQUERY -->
+<script src="./node_modules/jquery/dist/jquery.min.js" charset="utf-8"></script>
+<!-- //JQUERY -->
+
+<!-- BOOTSTRAP 4 -->
+<script src="./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js" charset="utf-8"></script>
+
+<!-- //BOOTSTRAP 4 -->
+
+<!-- METROUI -->
+<script src="https://cdn.metroui.org.ua/v4/js/metro.min.js"></script>
+<!-- //METROUI -->
+
+<!-- VUEJS -->
+<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+<!-- //VUEJS -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
